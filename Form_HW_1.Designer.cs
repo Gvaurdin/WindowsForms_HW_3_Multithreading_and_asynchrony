39ce197013ad9e636ba909f678b1a9bab2780c6c
Stanislav <gvaurdin777@yandex.ru>
2024-03-18 21:32:04 +0300

    Добавьте файлы проекта.

---
+++ Form_HW_1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsForms_HW_3_Multithreading_and_asynchrony
+{
+    partial class Form_HW_1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxCountProgressBars = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(152, 72);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxCountProgressBars
+            // 
+            this.textBoxCountProgressBars.Location = new System.Drawing.Point(223, 36);
+            this.textBoxCountProgressBars.Name = "textBoxCountProgressBars";
+            this.textBoxCountProgressBars.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCountProgressBars.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter to count progress bars";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(300, 72);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 3;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = false;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // Form_HW_1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 352);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxCountProgressBars);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form_HW_1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form_HW_1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBoxCountProgressBars;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonStop;
+    }
+}
+
---
+++ Form_HW_1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_HW_3_Multithreading_and_asynchrony
+{
+    public partial class Form_HW_1 : Form
+    {
+        private List<ProgressBar> progressBars;
+        private Thread thread;
+        private int count;
+        private ManualResetEvent resetEvent = new ManualResetEvent(true);
+        public Form_HW_1()
+        {
+            InitializeComponent();
+            progressBars = new List<ProgressBar>();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            resetEvent.Set();
+            count = int.Parse(textBoxCountProgressBars.Text);
+            if (count > 5)
+            {
+                MessageBox.Show("Enter to correct value progress bars ( MAX 5)");
+                return;
+            }
+            else if (progressBars == null || count != progressBars.Count)
+            {
+                int height = 70;
+                for (int i = 0; i < count; i++)
+                {
+                    ProgressBar progressBar = new ProgressBar();
+                    progressBar.Parent = this;
+                    progressBar.Visible = true;
+                    progressBar.Width = 200;
+                    progressBar.Height = 30;
+                    height += 40;
+                    progressBar.Location = new Point(200, height);
+                    progressBars.Add(progressBar);
+                }
+            }
+            thread = new Thread(() => ChangeValueProgressBars());
+            thread.Start();
+        }
+
+        private void ChangeValueProgressBars()
+        {
+            Random random = new Random();
+            while (true)
+            {
+                foreach (var item in progressBars)
+                {
+                    if (item.InvokeRequired)
+                    {
+                        item.BeginInvoke(new Action(delegate ()
+                        {
+                            int value = random.Next(item.Minimum, item.Maximum + 1);
+                            Color randomColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                            item.Value = value;
+                            item.Style = ProgressBarStyle.Continuous;
+                            item.ForeColor = randomColor;
+                        }));
+                    }
+                }
+                Thread.Sleep(200);
+                resetEvent.WaitOne();
+            }
+
+
+
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            resetEvent.Reset();
+        }
+
+        private void Form_HW_1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (thread != null && thread.IsAlive)
+            {
+                thread.Abort();
+            }
+        }
+    }
+}
